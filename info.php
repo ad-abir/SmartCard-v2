b02b35553e6451b8cb57515443860867ab16fd26
ad-abir <abhishek.das0023@gmail.com>
2024-04-16 18:20:20 +0600

    connection

--- info.php
+++ info.php
@@ -4,6 +4,9 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Contact Information Form</title>
+
+<link rel="stylesheet" href="info.css">
+
 </head>
 <body>
 <h2>Contact Information</h2>

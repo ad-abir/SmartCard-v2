9d2cc8e4636dc7c923aa1a4c0f2ff216d1fb9bec
ad-abir <abhishek.das0023@gmail.com>
2024-04-17 22:33:56 +0600

    small change

--- info.php
+++ info.php
@@ -31,26 +31,30 @@
         <label for="whatsapp_number">WhatsApp Number:</label><br>
         <input type="tel" id="whatsapp_number" name="whatsapp_number" pattern="[0-9]{10}"><br><br>
 
-        <label for="home_address">Home Address with Name:</label><br>
-        <textarea id="home_address" name="home_address" rows="4" cols="50"></textarea><br><br>
+        <label for="home_address">Home Address:</label><br>
+        <input type="text" id="home_address_name" name="home_address_name" placeholder="Name"><br>
+        <textarea id="home_address" name="home_address" rows="4" cols="50" placeholder="Address"></textarea><br>
+        <input type="url" id="home_map_link" name="home_map_link" placeholder="Google Maps Link"><br><br>
 
-        <label for="company_address">Company Address with Name:</label><br>
-        <textarea id="company_address" name="company_address" rows="4" cols="50"></textarea><br><br>
+        <label for="company_address">Company Address:</label><br>
+        <input type="text" id="company_address_name" name="company_address_name" placeholder="Name"><br>
+        <textarea id="company_address" name="company_address" rows="4" cols="50" placeholder="Address"></textarea><br>
+        <input type="url" id="company_map_link" name="company_map_link" placeholder="Google Maps Link"><br><br>
 
         <label for="emergency_number">Emergency Number:</label><br>
         <input type="tel" id="emergency_number" name="emergency_number" pattern="[0-9]{10}" required><br><br>
 
         <label for="facebook_link">Facebook Link:</label><br>
-        <input type="url" id="facebook_link" name="facebook_link"><br><br>
+        <input type="url" id="facebook_link" name="facebook_link" placeholder="Facebook Link"><br><br>
 
         <label for="instagram_link">Instagram Link:</label><br>
-        <input type="url" id="instagram_link" name="instagram_link"><br><br>
+        <input type="url" id="instagram_link" name="instagram_link" placeholder="Instagram Link"><br><br>
 
         <label for="linkedin_link">LinkedIn Link:</label><br>
-        <input type="url" id="linkedin_link" name="linkedin_link"><br><br>
+        <input type="url" id="linkedin_link" name="linkedin_link" placeholder="LinkedIn Link"><br><br>
 
         <label for="github_link">GitHub Link:</label><br>
-        <input type="url" id="github_link" name="github_link"><br><br>
+        <input type="url" id="github_link" name="github_link" placeholder="GitHub Link"><br><br>
 
         <input type="submit" value="Submit">
     </form>
